18e3722d49d736cb1d2494189a7ddea697f4c4f5
mahaltrupti <truptimahal2004@gmail.com>
2025-09-27 19:40:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
 //your JS code here. If required.
 const panels = document.querySelectorAll('.panel');
 
-// Add a click event listener to each panel
 panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        // Remove the 'active' class from the currently active panel
-        removeClass();
-        
-        // Add the 'active' class to the clicked panel
-        panel.classList.add('active');
-    });
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
 });
 
-// A helper function to remove the 'active' class from all panels
-function removeClass() {
-   panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        console.log('Clicked Panel:', panel.id); // Check which panel is clicked
-        removeClass();
-        panel.classList.add('active');
-    });
-});
+function removeActiveClasses() {
+  panels.forEach(panel => panel.classList.remove('active'));
 }
